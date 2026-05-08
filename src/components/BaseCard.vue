9d53196919f0375a0cc932b0c5b1ae39cf9de002
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-05-08 09:48:35 +0200

    feat: add BaseCard component

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts"></script>
+
+<template>
+  <article class="card">
+    <header class="card__header">
+      <slot name="header"></slot>
+    </header>
+
+    <div class="card__body">
+      <slot></slot>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+.card {
+  padding: 20px 32px;
+  background-color: var(--primary-bg-color);
+  border-radius: 16px;
+}
+
+.card__header {
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 20px;
+}
+
+.card__body {
+  padding: 20px 0;
+}
+</style>
