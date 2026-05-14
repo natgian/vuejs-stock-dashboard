910eb4c6b004abecf82a6dd4c96db39551c530c2
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-05-14 13:11:01 +0200

    refactor: modify baseURL and add params in axios instance, rename fetchData to fetchCompanyData, adjust endpoint

--- src/services/stockService.ts
+++ src/services/stockService.ts
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_SHEETDB_BASE_URL,
+  baseURL: import.meta.env.VITE_GOOGLESHEETS_BASE_URL,
+  params: {
+    key: import.meta.env.VITE_GOOGLESHEETS_API_KEY,
+  },
 });
 
-export async function fetchData(endpoint: string = "") {
+export async function fetchCompanyData(sheetName: string) {
   try {
-    const { data } = await api.get(`/${endpoint}`);
+    const { data } = await api.get(`/values/$${sheetName}!3:1000`);
     return data;
   } catch (error) {
-    console.error("fetching stocks failed:", error);
+    console.error("fetching data failed:", error);
     throw error;
   }
 }
